7bf654076c9cdf92e22b0eacaf04bb8ba809dc70
Sander Vassel <sander.vassel@gmail.com>
2025-11-04 17:40:03 +0200

    Alustus

---
+++ H1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Give a number: " << endl; // Kysytään käyttäjältä numero
+    cin >> num1; // Lukee käyttäjän inputin
+    cout << "Give another number: " << endl; // Kysyy toista numeroa käyttäjältä
+    cin >> num2; // Lukee käyttäjän inputin
+    return 0;
+}
