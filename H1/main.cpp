eafb700e73e2e047922a6e4268a02b1aae024845
Sander Vassel <sander.vassel@gmail.com>
2025-11-04 21:53:45 +0200

    Lisätty calcDiv funktio

--- H1/main.cpp
+++ H1/main.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 void calcSum (int x, int y) {
-    cout << "Total is: " << x + y << endl;
+    cout << "Total is: " << x + y << endl; // Tulostaa plus laskun
+}
+void calcDiv (int x, int y) {
+    if (y==0) {
+        cout << "Error, divider cant be a zero" << endl; // Virheilmoitus jos jakaja on 0
+    } else {
+    cout << "Divison is: " << x / y << endl; // Tulostaa jakolaskun
+    }
 }
 
 int main()
@@ -14,5 +21,6 @@ int main()
     cout << "Give another number: " << endl; // Kysyy toista numeroa käyttäjältä
     cin >> num2; // Lukee käyttäjän inputin
     calcSum(num1, num2); // Laskee yhteen
+    calcDiv(num1, num2); // Tekee jakolaskun annetuilla numeroilla
     return 0;
 }
